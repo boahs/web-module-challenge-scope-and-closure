012ebb0d33efa1dc717d7c73c300b680148b7afe
boahs <39522778+boahs@users.noreply.github.com>
2020-07-20 14:56:14 -0400

    task 2

--- index.js
+++ index.js
@@ -61,13 +61,14 @@ function counter2() {
 
 Write a function called `inning` that returns a random number of points that a team scored in an inning. This should be a whole number between 0 and 2. */
 
-function inning(/*Code Here*/) {
-  /*Code Here*/
+function inning() {
+  return Math.floor(Math.random() * Math.floor(3));
 }
 
 /* Task 3: finalScore()
 
-Write a higher order function called `finalScore` that accepts the callback function `inning` (from above) and a number of innings and and returns the final score of the game in the form of an object.
+Write a higher order function called `finalScore` that accepts the callback function `inning` (from above) and a number 
+of innings and and returns the final score of the game in the form of an object.
 
 For example, 
 
@@ -79,9 +80,7 @@ finalScore(inning, 9) might return:
 
 */
 
-function finalScore(/*code Here*/) {
-  /*Code Here*/
-}
+function finalScore(callback) {}
 
 /* Task 4: 
 
